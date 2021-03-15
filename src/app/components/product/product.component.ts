aa42205913ca1efcad3c4c5c4931c778124ddb4b
maliyalcin <maliyalcin08@gmail.com>
2021-03-15 23:13:49 +0300

    services added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,53 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  product1: any = {
-    productId: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2: any = {
-    productId: 2,
-    productName: 'Kalem',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3: any = {
-    productId: 3,
-    productName: 'Kamera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4: any = {
-    productId: 4,
-    productName: 'Hesap',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5: any = {
-    productId: 5,
-    productName: 'Kulaklık',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
-  constructor() { }
+  products: Product[] = [];
+  dataLoaded = false;
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts() {
+    this.productService.getProducts().subscribe(response=>{
+      this.products=response.data;
+      this.dataLoaded = true;
+    })
+  }
 }
